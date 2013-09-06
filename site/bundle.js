0bbddfe19ea145ec9bd59badb5609fe9ebe28bb0
jhnkllydev <jhnklly.dev@gmail.com>
2013-09-05 19:20:56 -0700

    new init zoom 14

--- site/bundle.js
+++ site/bundle.js
@@ -1,7 +1,7 @@
 ;(function(e,t,n){function r(n,i){if(!t[n]){if(!e[n]){var s=typeof require=="function"&&require;if(!i&&s)return s(n,!0);throw new Error("Cannot find module '"+n+"'")}var o=t[n]={exports:{}};e[n][0](function(t){var i=e[n][1][t];return r(i?i:t)},o,o.exports)}return t[n].exports}for(var i=0;i<n.length;i++)r(n[i]);return r})({1:[function(require,module,exports){
 var leafletPip = require('../'),
     //map = L.map('map').setView([37.8, -96], 4),
-    map = L.map('map').setView([37.7837,-122.4166], 12),
+    map = L.map('map').setView([37.7837,-122.4166], 14),
     gjLayer = L.geoJson(statesData, {
 		style: style,
 		onEachFeature: mouseHandlers
